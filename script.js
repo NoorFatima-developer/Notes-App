7dab7dd70ac3ae194a955512e6b8044258f0a71b
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-25 06:53:32 +0500

    code is added

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 const notescontainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
-const notes = document.querySelector(".input-box");
+let notes = document.querySelectorAll(".input-box");
+
+function showNodes(){
+    notescontainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNodes();
+function updateStorage() {
+    localStorage.setItem("notes", notescontainer.innerHTML);
+
+}
 
 createBtn.addEventListener("click", () => {
-    let inputBox = document.createElement("p");
+    let inputBox = document.createElement("P");
     let img = document.createElement("img");
     inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
     img.src = "./images/delete.png";
     notescontainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+});
+
+notescontainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box")
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+
+        document.addEventListener("keydown", event => {
+            if(event.key === "Enter"){
+                document.execCommand("insertLineBreak");
+                event.preventDefault()
+            }
+        })
+    }
 });
